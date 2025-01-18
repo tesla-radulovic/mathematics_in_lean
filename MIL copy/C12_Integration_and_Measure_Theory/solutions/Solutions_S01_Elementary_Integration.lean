d1a5c5eea4d23bb6ba7877690608069541d8d7e3
tesla-radulovic <112297193+tesla-radulovic@users.noreply.github.com>
2025-01-18 20:32:17 +0000

    made copy

---
+++ MIL copy/C12_Integration_and_Measure_Theory/solutions/Solutions_S01_Elementary_Integration.lean
@@ -0,0 +1,11 @@
+import MIL.Common
+import Mathlib.MeasureTheory.Integral.IntervalIntegral
+import Mathlib.Analysis.SpecialFunctions.Integrals
+import Mathlib.Analysis.Convolution
+
+open Set Filter
+
+open Topology Filter
+
+noncomputable section
+
